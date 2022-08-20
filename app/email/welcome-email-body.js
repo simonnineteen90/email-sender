b263133899001b3983bbfb4bf314f138f573f52c
Simon Dunn <simondunn90@gmail.com>
2022-08-20 08:07:33 +0100

    initial setup

---
+++ app/email/welcome-email-body.js
@@ -0,0 +1,7 @@
+module.exports = {
+    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    to: "bar@example.com, baz@example.com", // list of receivers
+    subject: "Welcome", // Subject line
+    text: "Welcome email", // plain text body
+    html: "<b>Welcome email</b>", // html body
+  }
